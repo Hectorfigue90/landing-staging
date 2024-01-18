725766f2846d278a7aabc71a9d8eec64ea2a2eb2
saqlainabbas777 <msaqlainabbas3@gmail.com>
2024-01-18 18:26:42 +0500

    link added

--- src/Components/Home/Publications/index.jsx
+++ src/Components/Home/Publications/index.jsx
@@ -23,6 +23,10 @@ const Publications = () => {
 		};
 	}, []);
 
+	const handleLink = (link) => {
+		window.open(link);
+	};
+
 	return (
 		<div className="publication-container">
 			<div className="publication-text-container">
@@ -30,7 +34,9 @@ const Publications = () => {
 					Publications&nbsp;&<span className="color-inlineVariation-blue">Press</span>
 				</p>
 			</div>
-			<div className="publication-content-upper-container">
+			<div className="publication-content-upper-container" onClick={() => {
+				handleLink("https://www.einpresswire.com/article/682102004/chemify-and-prepaire-labs-partner-to-apply-chemistry-ai-robotics-to-accelerate-discovery-of-non-addictive-opioids");
+			}}>
 				<div className='publication-image'>
 					<img src={windowWidth >= 1256 ? publication1: publicationSmall} alt="publication-1" className="publication-image-1" />
 				</div>
@@ -47,7 +53,9 @@ const Publications = () => {
 				</div>
 			</div>
 			<div className="publication-lower-container">
-				<div className="small-publication-container">
+				<div className="small-publication-container" onClick={() => {
+					handleLink("https://www.einpresswire.com/article/637477160/prepaire-labs-acquires-state-of-the-art-3d-bioprinter-from-cellink-for-new-uae-research-lab");
+				}}>
 					<div className="publication-2-img-container">
 						<img src={publication2} className="img-responsive" />
 					</div>
@@ -57,7 +65,9 @@ const Publications = () => {
 						<p className="publication-small-date">August 14, 2023</p>
 					</div>
 				</div>
-				<div className="small-publication-container">
+				<div className="small-publication-container" onClick={() => {
+					handleLink("https://kalkinemedia.com/news/world-news/prepaire-labs-and-syndesis-announce-strategic-partnership-to-transform-global-health-outcomes");
+				}}>
 					<div className="publication-2-img-container">
 						<img src={publication3} className="img-responsive" />
 					</div>
@@ -67,7 +77,9 @@ const Publications = () => {
 						<p className="publication-small-date">June 05, 2023</p>
 					</div>
 				</div>
-				<div className="small-publication-container">
+				<div className="small-publication-container" onClick={() => {
+					handleLink("https://www.ktsm.com/business/press-releases/ein-presswire/638930046/prepaire-labs-receives-two-patents-for-new-anti-viral-drug/");
+				}}>
 					<div className="publication-2-img-container">
 						<img src={publication4} className="img-responsive" />
 					</div>
