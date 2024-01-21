bdf83fbcf1c7e2ec3e40e6fd3a65af9e8c1a32c0
kunj_m <kunj.iitr@gmail.com>
2024-01-21 16:44:17 +0530

    toxi-free-banner-changed

--- src/Components/Home/ToxiFree/index.jsx
+++ src/Components/Home/ToxiFree/index.jsx
@@ -12,7 +12,7 @@ const ToxiFree = () => {
 			<div className="text-frame-discoverMainCards">
 				<div className="banner-heading">
 					<img src={toxifreeIcon} alt="toxifree" />
-					<h2>Toxifree the app to combat</h2>
+					<h2>The Toxifree(tm) app to combat</h2>
 					<h2 className="blue">drug addiction</h2>
 				</div>
 				<p>
