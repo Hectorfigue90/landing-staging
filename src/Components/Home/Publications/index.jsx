2f2781fe618fa34f5b5fdc2d1ded243fb968d156
Hector Figueroa <hectorfigueroa3d@gmail.com>
2024-02-04 11:24:54 -0500

    date

--- src/Components/Home/Publications/index.jsx
+++ src/Components/Home/Publications/index.jsx
@@ -81,7 +81,7 @@ const Publications = () => {
 							Prepaire Labs today announced a groundbreaking initiative to
 							achieve carbon neutrality.
 						</p>
-						<p className="publication-small-date">January 04, 2024</p>
+						<p className="publication-small-date">February 04, 2024</p>
 					</div>
 				</div>
 				<div
