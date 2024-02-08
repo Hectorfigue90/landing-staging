dfac861cb5aafac8741d04863a6366d3c596b4e1
kunj_m <kunj.iitr@gmail.com>
2024-02-08 13:51:43 +0530

    new-ui-fix

--- src/Components/Home/Publications/index.jsx
+++ src/Components/Home/Publications/index.jsx
@@ -363,7 +363,7 @@ const Publications = () => {
 							Prepaire Labs today announced a groundbreaking initiative to
 							achieve carbon neutrality.
 						</p>
-						<p className="publication-press-date">February 04, 2024</p>
+						{/* <p className="publication-press-date">February 04, 2024</p> */}
 					</div>
 					<p className="publication-press-date">January 18, 2024</p>
 				</div>
@@ -387,7 +387,7 @@ const Publications = () => {
 							Chemify Partners with Prepaire Labs to Apply Ground-breaking
 							Chemistry AI Robotics.
 						</p>
-						<p className="publication-small-date">January 18, 2024</p>
+						{/* <p className="publication-small-date">January 18, 2024</p> */}
 					</div>
 					<p className="publication-small-date">August 14, 2023</p>
 				</div>
