7f60f3f02ff09e1aa5abdaa8042ed20a5beeaf4d
Hector Figueroa <hectorfigueroa3d@gmail.com>
2024-03-07 09:41:19 -0500

    remoove team

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Developers from "./Pages/Developers";
 import Mission from "./Pages/Mission";
 import Biotune from "./Pages/Biotune";
 import CrowdfundingPage from "./Pages/CrowdFunding";
-import Team from "./Pages/Team";
+// import Team from "./Pages/Team";
 import Hiring from "./Pages/Hiring";
 function App() {
 	return (
@@ -17,8 +17,8 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
 				<Route exact path="/developers" element={<Developers />} />
-				<Route exact path="/team" element={<Team />} />
-				<Route exact path="/hiring" element={<Hiring/>} />
+				{/* <Route exact path="/team" element={<Team />} /> */}
+				<Route exact path="/hiring" element={<Hiring />} />
 				<Route exact path="/mission" element={<Mission />} />
 				<Route exact path="/biotune" element={<Biotune />} />
 				<Route exact path="/crowdfunding" element={<CrowdfundingPage />} />
